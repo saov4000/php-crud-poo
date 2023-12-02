cd5071c500f1d0c139260f7dc65c7b0a88fa9cca
saov4000 <saov4000@gmail.com>
2023-12-02 10:24:28 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-light bg-light menu">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                CRUD PHP POO
+            </a>
+        </div>
+    </nav>
+    <br>
+    <div class="container">
+        <a href="" class="btn btn-primary">Cadastrar</a>
+        <br>
+        <br>
+        <table class="table table-bordered table-hover table-light">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>Sexo</th>
+                    <th>Cidade</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>teste</td>
+                    <td>teste</td>
+                    <td>teste</td>
+                    <td>teste</td>
+                    <td class="text-center">
+                        <a href="http://" class="btn btn-warning btn-sm">Editar</a>
+                        <a href="" class="btn btn-danger btn-sm">Excluir</a>
+                    </td>
+                </tr> 
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
